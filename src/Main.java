ce9db5487e9151256cb26ae20675b390401f431d
Dias Kamza <68639981+kamzadias@users.noreply.github.com>
2021-10-18 00:04:49 +0600

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,9 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
-        GadgetFactory appleGadgetFactory = new AppleGadgetFactory();
-        GadgetFactory samsungGadgetFactory = new SamsungGadgetFactory();
+        PhoneFactory factory = new SamsungPhoneFactory();
 
-        Smartphone iPhone13 = appleGadgetFactory.createSmartphone(Smartphone.SmartphoneSeries.iPhone);
-        iPhone13.getInfo();
-        System.out.println("-------------------------------------");
-
-        Smartphone galaxyS21 = samsungGadgetFactory.createSmartphone(Smartphone.SmartphoneSeries.GalaxyS);
-        galaxyS21.getInfo();
-
-        System.out.println("*****************************************************************************************");
-
-        Tablet iPadAir = appleGadgetFactory.createTablet(Tablet.TabletSeries.iPad);
-        iPadAir.getInfo();
-        System.out.println("-------------------------------------");
-
-        Tablet galaxyTabS7 = samsungGadgetFactory.createTablet(Tablet.TabletSeries.GalaxyTab);
-        galaxyTabS7.getInfo();
+        Phone phone = factory.createPhone();
+        phone.make();
     }
 }
