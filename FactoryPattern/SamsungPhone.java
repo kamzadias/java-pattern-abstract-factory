f083aba25b255a389ef7299becbfa953df24a35a
Dias Kamza <68639981+kamzadias@users.noreply.github.com>
2021-10-18 00:07:08 +0600

    Add files via upload

---
+++ FactoryPattern/SamsungPhone.java
@@ -0,0 +1,7 @@
+public class SamsungPhone implements Phone{
+
+    @Override
+    public void make() {
+        System.out.println("make Samsung phones with Android OS");
+    }
+}
---
+++ FactoryPattern/iPhone.java
@@ -0,0 +1,7 @@
+public class iPhone implements Phone{
+
+    @Override
+    public void make() {
+        System.out.println("make iPjone with IOS operating system");
+    }
+}
